6324ad33f63dc6f6a93267e151baeb9cd8ef0987
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2025-03-11 16:17:39 +0330

    added the rest of the js

--- main.js
+++ main.js
@@ -3,21 +3,27 @@ const nameError = document.querySelectorAll(".error-message");
 const firstName = document.getElementById("FirstName");
 const lastName = document.getElementById("lastName");
 const emailInput = document.getElementById("email");
-const selectedOption = document.querySelector('input[name="query-type"]:checked');
 const messsageInput = document.getElementById("message");
 const checkBox = document.getElementById("agreeing");
+const successContainer = document.querySelector(".success-container");
 
 
 
 
 submitBtn.addEventListener("click", (event)=> {
   event.preventDefault();
-  handleFirstName();
-  handleLastName();
-  handelEmail();
-  handleQueryType();
-  handleMessage();
-  handelCheckBox();
+  const isFirstNameValid = handleFirstName();
+  const isLastNameValid = handleLastName();
+  const isEmailValid = handelEmail();
+  const isQueryTypeValid = handleQueryType();
+  const isMessageValid = handleMessage();
+  const isCheckBoxValid = handelCheckBox();
+
+  if (isFirstNameValid && isLastNameValid && isEmailValid && isQueryTypeValid && isMessageValid &&isCheckBoxValid){
+    successContainer.style.display = "block";
+  } else{
+    successContainer.style.display = "none";
+  }
 });
 
 
@@ -30,9 +36,11 @@ const errorMessage = nameError[0];
  if (firstNameValue.length < 3 || !isNaN(firstNameValue)) {
    errorMessage.style.display = "block";
    firstName.classList.add(`error-border`);
+   return false;
  } else {
   errorMessage.style.display = "none";
   firstName.classList.remove(`error-border`);
+  return true;
  }
 }
 
@@ -44,9 +52,11 @@ function handleLastName() {
   if (lastNameValue.length < 4 || !isNaN(lastNameValue)) {
     errorMessage.style.display = "block";
     lastName.classList.add(`error-border`);
+    return false;
   } else {
     errorMessage.style.display = "none";
     lastName.classList.remove(`error-border`);
+    return true;
   }
 }
 
@@ -59,47 +69,56 @@ function handelEmail() {
   if(!isValidEmail) {
     errorMessage.style.display = "block";
     emailInput.classList.add('error-border');
+    return false;
   } else {
-    errorMessage.style.display = "none";
-    emailInput.classList.remove(`error-border`);
+      errorMessage.style.display = "none";
+      emailInput.classList.remove(`error-border`);
+      return true;
+    }
+  
   }
-
-}
-
-
-function handleQueryType() {
-  const errorMessage = nameError[3];
-
-  if(selectedOption) {
-    errorMessage.style.display = "none";
-  } else {
-    errorMessage.style.display = "block";
+  
+  
+  function handleQueryType() {
+    const selectedOption = document.querySelector('input[name="query-type"]:checked');
+    const errorMessage = nameError[3];
+  
+    if(selectedOption) {
+      errorMessage.style.display = "none";
+      return true;
+    } else {
+      errorMessage.style.display = "block";
+      return false;
+    }
   }
-}
-
-function handleMessage() {
-  const message = messsageInput.value.trim();
-  const errorMessage = nameError[4];
-
-  if(message.length < 13) {
-    errorMessage.style.display = "block";
-    messsageInput.classList.add(`error-border`);
-  } else {
-    errorMessage.style.display = "none";
-    messsageInput.classList.remove(`error-border`);
+  
+  function handleMessage() {
+    const message = messsageInput.value.trim();
+    const errorMessage = nameError[4];
+  
+    if(message.length < 13) {
+      errorMessage.style.display = "block";
+      messsageInput.classList.add(`error-border`);
+      return false;
+    } else {
+      errorMessage.style.display = "none";
+      messsageInput.classList.remove(`error-border`);
+      return true;
+    }
   }
-}
-
-
-function handelCheckBox() {
-  const errorMessage = nameError[5];
-
-  if (checkBox.checked) {
-    errorMessage.style.display = "none";
-    checkBox.classList.remove(`error-border`);
-  } else {
-    errorMessage.style.display = "block";
-    checkBox.classList.remove(`error-border`);
+  
+  
+  function handelCheckBox() {
+    const errorMessage = nameError[5];
+  
+    if (checkBox.checked) {
+      errorMessage.style.display = "none";
+      checkBox.classList.remove(`error-border`);
+      return true;
+    } else {
+      errorMessage.style.display = "block";
+      checkBox.classList.remove(`error-border`);
+      return false;
+    }
   }
-}
-
+  
